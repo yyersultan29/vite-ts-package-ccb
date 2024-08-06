a0042ed0c680aa0bad924b006b4e6319b4fe69f4
yyersultan29 <yersultanyerkinbek01@gmail.com>
2024-08-06 21:24:58 +0500

    relase config ch

--- release.config.js
+++ release.config.js
@@ -1,13 +1,11 @@
-{
-  "branches": [
-    "master"
+module.exports = {
+  branches: ['master'],
+  repositoryUrl: 'https://github.com/yyersultan29/vite-ts-package-ccb',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
   ],
-  "repositoryUrl": "https://github.com/yyersultan29/vite-ts-package-ccb",
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
-    "@semantic-release/github"
-  ]
-}
+};

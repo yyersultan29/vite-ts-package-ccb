4e86a2a4c1a73d1a1cbed6f9e2dd3d6631abf099
yyersultan29 <yersultanyerkinbek01@gmail.com>
2024-08-06 21:00:05 +0500

    done

--- release.config.js
+++ release.config.js
@@ -1,13 +1,10 @@
-/**
- * @type {import('semantic-release').GlobalConfig}
- */
-export const branches = ['master'];
-export const repositoryUrl =
-  'https://github.com/yyersultan29/vite-ts-package-ccb';
-export const plugins = [
-  '@semantic-release/commit-analyzer',
-  '@semantic-release/release-notes-generator',
-  '@semantic-release/changelog',
-  '@semantic-release/npm',
-  '@semantic-release/github',
-];
+module.exports = {
+  branches: ['feature/*', 'release/*', 'bugfix/*'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+};
